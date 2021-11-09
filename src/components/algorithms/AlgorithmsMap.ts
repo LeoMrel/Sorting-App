3063ed8b066b440b48854e4991b288679034c11e
LeoMrel <leonardomrel0028@gmail.com>
2021-11-09 18:45:11 -0300

    finished project

--- src/components/algorithms/AlgorithmsMap.ts
+++ src/components/algorithms/AlgorithmsMap.ts
@@ -6,9 +6,9 @@ import selectionSort from "./SelectionSort";
 
 
 export const algorithmsMap: {name: string, function: any, complexity: string}[] = [
-    {name: "BubbleSort", function: bubbleSort, complexity: 'O(n^2)'},
-    {name: 'SelectionSort', function: selectionSort, complexity: 'O(n^2)'},
-    {name: 'InsertionSort', function: insertionSort, complexity: 'O(n^2)'},
-    {name: 'MergeSort', function: mergeSort, complexity: 'O(log(n))'},
-    {name: "QuickSort", function: quickSort, complexity: 'O(n^2)'}
+    {name: "BubbleSort", function: bubbleSort, complexity: 'θ(n^2)'},
+    {name: 'SelectionSort', function: selectionSort, complexity: 'θ(n^2)'},
+    {name: 'InsertionSort', function: insertionSort, complexity: 'θ(n^2)'},
+    {name: 'MergeSort', function: mergeSort, complexity: 'θ(log(n))'},
+    {name: "QuickSort", function: quickSort, complexity: 'θ(log(n))'}
 ]
